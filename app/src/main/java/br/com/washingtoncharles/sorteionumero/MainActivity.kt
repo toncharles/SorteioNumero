53c0838b5b12b4ef86e56e776b277b6fb96a1dd3
Washington Charles de O Moraes <toncharles@live.com>
2020-02-25 09:15:10 -0300

    adicionando evento no button

--- app/src/main/java/br/com/washingtoncharles/sorteionumero/MainActivity.kt
+++ app/src/main/java/br/com/washingtoncharles/sorteionumero/MainActivity.kt
@@ -2,11 +2,21 @@ package br.com.washingtoncharles.sorteionumero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun cliqueBotao(view: View){
+        //Log.i("BOTAO","Botão foi pressionado")
+        var texto = findViewById(R.id.textoExibicao) as TextView
+        texto.setText("Texto alterado")
+
     }
 }
